87484c0a32e6d80b842b830b38d00bd39f16787f
Chan, Aloysius <dr.aloysius@gmail.com>
2019-05-08 23:42:48 +1000

    - Added top level components and mat-tab-nav-bar for navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SimulateComponent } from './simulate/simulate.component';
+import { LogsComponent } from './logs/logs.component';
+import { MonitorComponent } from './monitor/monitor.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'simulate', component: SimulateComponent },
+  { path: 'monitor', component: MonitorComponent },
+  { path: 'logs', component: LogsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTabsModule } from '@angular/material';
+import { SimulateComponent } from './simulate/simulate.component';
+import { MonitorComponent } from './monitor/monitor.component';
+import { LogsComponent } from './logs/logs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SimulateComponent,
+    MonitorComponent,
+    LogsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatTabsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
